39b61186967ca42c9cbe01f413d3f5d99ae126f5
Giacomo Amoroso <giacomo@amoroso.me>
2024-07-28 23:07:26 +0200

    Add H2 database

---
+++ README.md
@@ -0,0 +1,5 @@
+# Product Service
+
+**Access H2 Web Console**
+
+http://localhost:8082/products-service/h2-console
---
+++ src/main/java/com/appsdeveloperblog/estore/ProductService/query/ProductEventsHandler.java
@@ -0,0 +1,28 @@
+package com.appsdeveloperblog.estore.ProductService.query;
+
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+import com.appsdeveloperblog.estore.ProductService.core.data.ProductEntity;
+import com.appsdeveloperblog.estore.ProductService.core.data.ProductsRepository;
+import com.appsdeveloperblog.estore.ProductService.core.events.ProductCreatedEvent;
+
+@Component
+public class ProductEventsHandler {
+
+  private final ProductsRepository productsRepository;
+
+  public ProductEventsHandler(ProductsRepository productsRepository) {
+    this.productsRepository = productsRepository;
+  }
+
+  @EventHandler
+  public void on(ProductCreatedEvent event) {
+    
+    ProductEntity productEntity = new ProductEntity();
+    BeanUtils.copyProperties(event, productEntity);
+
+    productsRepository.save(productEntity);
+  }
+}
