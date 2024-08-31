2772ef9681d662b3be2a70fd48e584ec0afd63ce
Giacomo Amoroso <giacomo@amoroso.me>
2024-08-31 21:40:16 +0200

    WIP 92 Persisting product

--- src/main/java/com/appsdeveloperblog/estore/ProductService/command/ProductLookupEventsHandler.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductService/command/ProductLookupEventsHandler.java
@@ -4,14 +4,24 @@ import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
 
+import com.appsdeveloperblog.estore.ProductService.core.data.ProductLookupEntity;
+import com.appsdeveloperblog.estore.ProductService.core.data.ProductLookupRepository;
 import com.appsdeveloperblog.estore.ProductService.core.events.ProductCreatedEvent;
 
 @Component
 @ProcessingGroup("product-group")
 public class ProductLookupEventsHandler {
 
+  private final ProductLookupRepository productLookupRepository;
+
+  public ProductLookupEventsHandler(ProductLookupRepository productLookupRepository) {
+    this.productLookupRepository = productLookupRepository;
+  }
+
   @EventHandler
   public void on(ProductCreatedEvent event) {
-
+    ProductLookupEntity productLookupEntity = new ProductLookupEntity(event.getProductId(), event.getTitle());
+    
+    productLookupRepository.save(productLookupEntity);
   }
 }
