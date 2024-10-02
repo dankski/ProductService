aed49b47e5ca825652108c568ba32c595069590c
Giacomo Amoroso <giacomo@amoroso.me>
2024-10-02 23:03:11 +0200

    Add core dependency

--- src/main/java/com/appsdeveloperblog/estore/ProductService/command/ProductAggregate.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductService/command/ProductAggregate.java
@@ -10,6 +10,7 @@ import org.axonframework.spring.stereotype.Aggregate;
 import org.springframework.beans.BeanUtils;
 
 import com.appsdeveloperblog.estore.ProductService.core.events.ProductCreatedEvent;
+import com.appsdeveloperblog.estore.core.commands.ReserveProductCommand;
 
 @Aggregate
 public class ProductAggregate {
@@ -41,6 +42,13 @@ public class ProductAggregate {
 
     AggregateLifecycle.apply(productCreatedEvent);
 
+  }
+
+  @CommandHandler
+  public void handle(ReserveProductCommand reserveProductCommand) {
+    if (quantity < reserveProductCommand.getQuantity()) {
+      throw new IllegalArgumentException("Insufficent number of items in stock");
+    }
   }
 
   /**
